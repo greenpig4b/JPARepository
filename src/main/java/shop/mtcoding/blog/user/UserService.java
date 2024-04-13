9ec749ac13e44c4c8ed63ee811b0c0d6282a1af3
김지훈 <greenpig4b@gmail.com>
2024-04-14 02:58:19 +0900

    로그인

--- src/main/java/shop/mtcoding/blog/user/UserService.java
+++ src/main/java/shop/mtcoding/blog/user/UserService.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.user;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,22 @@ import shop.mtcoding.blog._core.errors.exception.Exception404;
 public class UserService {
     private final UserJPARepository userJPARepo;
 
+
     //회원가입
     @Transactional
     public User join(UserRequest.JoinDTO reqDTO){
 
         return userJPARepo.save(reqDTO.toEntity());
+    }
+
+    //로그인
+    @Transactional
+    public User login(UserRequest.LoginDTO reqDTO){
+        // 1 아이디체크
+        User sessionUser = userJPARepo.findByIdAndPassword(reqDTO.getUsername(),reqDTO.getPassword())
+                .orElseThrow(() -> new Exception404("아이디 및 비밀번호가 일치하지않습니다."));
+
+        return sessionUser;
     }
 
     //회원수정 폼
